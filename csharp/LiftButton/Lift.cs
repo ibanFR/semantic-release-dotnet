4b8ede2cf1d761cb0e9482e2c108ee277754eef1
ibanFR <ibanFR@users.noreply.github.com>
2026-01-05 16:52:20 +0000

    feat: switch on the lights when button is pressed and doors are closed

--- csharp/LiftButton/Lift.cs
+++ csharp/LiftButton/Lift.cs
@@ -2,8 +2,26 @@
 
 public class Lift
 {
+    private string _lights;
+
+    public Lift()
+    {
+        _lights = "Off";
+    }
+
+
     public string Doors()
     {
         return "Closed";
+    }
+
+    public void PressButton()
+    {
+        _lights = "On";
+    }
+
+    public string Lights()
+    {
+        return _lights;
     }
 }
